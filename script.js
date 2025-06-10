d51ce9918c430bc507c000d5250ae025652dcbbc
Gaurav Meena <gauravmeena0708@gmail.com>
2025-06-11 00:22:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -673,7 +673,7 @@ function startGame() {
   playerTime = [300, 300];
   document.getElementById("winner").textContent = "";
   document.getElementById("currentTurn").textContent = "Current Turn: Player 1";
-  #drawBoard(board); // Note: This ideally should be drawBoardState
+  //drawBoard(board); // Note: This ideally should be drawBoardState
   drawBoardState(board);
   // renderGameTable(board); // Render the initial board in the table - REMOVED
   if (intervalId) clearInterval(intervalId);
